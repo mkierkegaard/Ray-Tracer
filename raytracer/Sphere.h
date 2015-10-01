3f00754ca81b20ad5fe6880a5f646f90286a2b08
Martin <marki423@student.liu.se>
2015-10-01 16:14:54 +0200

    ref fix

--- raytracer/Sphere.h
+++ raytracer/Sphere.h
@@ -1,16 +1,20 @@
-#pragma once
+#include <stdlib.h>
+#include <stdio.h>
 #include <glm/vec3.hpp>
 
-glm::vec3 position;
-float radius;
-glm::vec3 color;
+
 
 class Sphere
 {
+private:
+	glm::vec3 position;
+	float radius;
+	glm::vec3 color;
+
 public:
-	Sphere();
-	Sphere(glm::vec3 pos, float rad, glm::vec3 col);
-	~Sphere();
+	Sphere::Sphere();
+	Sphere::Sphere(glm::vec3 pos, float rad, glm::vec3 col);
+	Sphere::~Sphere();
 
 	
 };
