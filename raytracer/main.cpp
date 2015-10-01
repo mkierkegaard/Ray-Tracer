8b6d26be62cca554964aed2c62462b5b571747ad
Martin <marki423@student.liu.se>
2015-10-01 15:44:28 +0200

    main

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -1,21 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "Sphere.h"
 
-void writeImage() {
 
-	const int dimx = 800, dimy = 800;
+const int dimx = 800, dimy = 800;
+
+void writeImage() {	
+
 	int i, j;
 	FILE *fp = fopen("first.ppm", "wb"); /* b - binary mode */
 	(void)fprintf(fp, "P6\n%d %d\n255\n", dimx, dimy);
+
+	int** img = new int*[dimx];
+	for (int i = 0; i < dimx; ++i)
+		img[i] = new int[dimy];
+
 	for (j = 0; j < dimy; ++j)
 	{
 		for (i = 0; i < dimx; ++i)
 		{
+			img[j][i] = i*j;
 			static unsigned char color[3];
 			color[0] = i % 256;  /* red */
 			color[1] = j % 256;  /* green */
 			color[2] = (i * j) % 256;  /* blue */
-			(void)fwrite(color, 1, 3, fp);
+			(void)fwrite(img, 1, 3, fp);
 		}
 	}
 	(void)fclose(fp);
@@ -23,7 +32,7 @@ void writeImage() {
  
 int main(void)
 {
-
+	Sphere sphere = Sphere();
 	// Checking checking...
 	//houston plz
 	//plzzzzzzzzzzz
