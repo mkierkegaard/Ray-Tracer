a29997789b942fc15db03c0f09a14f954b097548
Martin <marki423@student.liu.se>
2015-10-02 10:59:09 +0200

    remove stuff

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -6,34 +6,6 @@
 
 const int dimx = 800, dimy = 800;
 
-void writeImage() {	
-
-	int i, j;
-	FILE *fp = fopen("first.ppm", "wb"); /* b - binary mode */
-	(void)fprintf(fp, "P6\n%d %d\n255\n", dimx, dimy);
-
-	int** img = new int*[dimx];
-	for (int i = 0; i < dimx; ++i)
-		img[i] = new int[dimy];
-
-	for (j = 0; j < dimy; ++j)
-	{
-		for (i = 0; i < dimx; ++i)
-		{
-
-			Ray ray = Ray(glm::vec3(0, 1, 2), glm::vec3(20, 20, 20));
-
-			img[j][i] = i*j;
-			static unsigned char color[3];
-			color[0] = i % 256;  /* red */
-			color[1] = j % 256;  /* green */
-			color[2] = (i * j) % 256;  /* blue */
-			(void)fwrite(img, 1, 3, fp);
-		}
-	}
-	(void)fclose(fp);
-}
-
 void render() {
 	for (int i = 0; i < dimx; i++) {
 		for (int j = 0; j < dimy; i++) {
